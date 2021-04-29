773eed54a08c243b10832cf1b7aab4a8bd370c2a
Jeevan Eashwar <jeevaneashwar2@gmail.com>
2021-04-29 12:14:16 +0530

    Renaming initial view controller

--- covid19/StateListViewController.swift
+++ covid19/StateListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StateListViewController.swift
 //  covid19
 //
 //  Created by Jeevan on 29/04/21.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StateListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
